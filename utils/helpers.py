ca08c4797a083a5a254dbbec2620a732424b8e37
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2025-05-05 17:08:44 +0200

    helpers.py [full restore to original functioning file]

---
+++ utils/helpers.py
@@ -0,0 +1,7 @@
+# Last updated: 2025-04-29 14:26:23
+def find_source_link(docs, keyword):
+    for doc in docs:
+        source = doc.metadata.get("source", "")
+        if keyword.lower() in source.lower():
+            return source
+    return None

1d8f662bd271c515e0064d2592e34a9533db3f3a
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2025-05-06 03:19:29 +0200

    Create langsmith connection.py

---
+++ Debuggers/langsmith connection.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+from langsmith import traceable
+
+load_dotenv()  # This loads your .env into os.environ
+
+@traceable
+def say_hi(name):
+    return f"Hello, {name}!"
+
+print(say_hi("Fabienne"))
